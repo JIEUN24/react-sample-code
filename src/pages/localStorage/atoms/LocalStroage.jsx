1df8ef7e035a48f84822b68d6b4cbd8c9200515f
lia <pandaa0628@gmail.com>
2022-10-26 18:21:31 +0900

    practice array

--- src/pages/localStorage/atoms/LocalStroage.jsx
+++ src/pages/localStorage/atoms/LocalStroage.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { useBeforeunload } from "react-beforeunload";
 
 const LocalStroage = () => {
-  const ref = useRef(null);
+  // const ref = useRef(null);
   // useBeforeunload((event) => event.preventDefault());
 
   const [category, setCategory] = useState({})
@@ -13,13 +13,29 @@ const LocalStroage = () => {
     { name: '매출3', amount: 0 },
   ])
   const [change, setChange] = useState({})
-  const [change1, setChange1] = useState([])
+  // const [change1, setChange1] = useState([])
 
+  const arr1 = [1, 2, 3];
+  const arr2 = [4, 5];
+  // const hello1 = arr2.map((item) => { return item })
+  arr1.push(...arr2)
+  console.log('arr----------->', arr1)
 
   const onChangeHandler = (e) => {
-    const { name, value } = e.target
-    setChange({ name: name, amount: value })
-    // setChange([...change, { name: name, amount: value }])
+    const { name, value } = e.target;
+    setData((prev) => {
+      // prev[name].amount = value;
+      console.log('prev-------->', prev)
+      return prev.map(v => {
+        let item = v;
+        if (item.name == name) {
+          item.amount = value;
+        }
+        return item;
+      })
+    })
+    // setChange({ name: name, amount: value })
+    // setChange({ ...change, [name]: value })
   }
 
   const hello = () => {
@@ -27,37 +43,39 @@ const LocalStroage = () => {
   }
 
   console.log(change)
-  console.log('change1', change1)
+  // console.log('change1', change1)
 
-  // const addCategory = (e) => {
-  //   const { value } = e.target;
-  //   setCategory({ ...category, name: value, amount: 0 })
-  // }
+  const addCategory = (e) => {
+    const { value } = e.target;
+    setCategory({ ...category, name: value, amount: 0 })
+  }
 
-  // const submitHandler = () => {
-  //   setData([...data, category])
-  // }
+  const submitHandler = () => {
+    setData([...data, category])
+  }
   // const click = () => {
   // }
 
   return (
     <div>
-      {data.map((item, idx) => {
+      {data.map((item) => {
+        console.log('---itme-->', item)
+
         return (
           <InputBox>
             <p>{item.name}</p>
-            <input type='number' name={item.name} onChange={onChangeHandler} ref={ref} onBlur={() => {
+            <input type='number' name={item.name} onChange={onChangeHandler} />
+            {/* <input type='number' name={item.name} onChange={onChangeHandler} ref={ref} onBlur={() => {
               setChange1([...change1, change])
               // console.log(change)
-            }} />
+            }} /> */}
           </InputBox>
         )
       })}
-      {/* <button onClick={click}>고</button> */}
       {/* <p>카테고리 추가</p> */}
-      {/* <input placeholder='카테고리 이름' onChange={addCategory} />
-      <button onClick={submitHandler}>제출</button> */}
-      {/* {data.map((item, idx) => {
+      <input placeholder='카테고리 이름' onChange={addCategory} />
+      <button onClick={submitHandler}>제출</button>
+      {data.map((item) => {
         return (
           <div>
             <p>{item.name}</p>
@@ -67,7 +85,7 @@ const LocalStroage = () => {
             }}>X</button>
           </div>
         )
-      })} */}
+      })}
     </div>
   )
 }
